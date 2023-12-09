cc07855e55d53be534dd73e9aa0c41ab3984ac52
vincent.vollers <vincent.vollers@minbzk.nl>
2023-12-09 11:50:07 +0100

    Added Day 9

---
+++ 2023/Day09/Solution.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Y2023.Day09;
+
+[ProblemName("Mirage Maintenance")]
+class Solution : Solver
+{
+    public IEnumerable<long> ZipDiff(IEnumerable<long> numbers) => numbers.Zip(numbers.Skip(1), (a, b) => b - a);
+
+    long RecurseRight(List<long> numbers) => numbers.Count == 0 ? 0 : (RecurseRight(ZipDiff(numbers).ToList()) + numbers.Last());
+    
+    long RecurseLeft(List<long> numbers) => numbers.Count == 0 ? 0 : (numbers.First() - RecurseLeft(ZipDiff(numbers).ToList()));
+    
+    List<List<long>> ParseNumbers(string input) => input.Split("\n").
+                                                         Select(line => line.Split(" ").
+                                                                             Select(long.Parse).
+                                                                             ToList()).
+                                                         ToList();
+    public object PartOne(string input) =>
+        ParseNumbers(input).
+            Select(RecurseRight).
+            Sum();
+   
+
+    public object PartTwo(string input) =>
+        ParseNumbers(input).
+            Select(RecurseLeft).
+            Sum();
+}

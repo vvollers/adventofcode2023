3e18c0bbf703317db1123c5edcf3280c2d4a7ae5
vincent.vollers <vincent.vollers@minbzk.nl>
2023-12-15 09:40:18 +0100

    Added Day15

--- 2023/Day14/Solution.cs
+++ 2023/Day14/Solution.cs
@@ -4,12 +4,25 @@ using adventofcode.AdventLib;
 
 namespace AdventOfCode.Y2023.Day14;
 
+public enum Orientation
+{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
+
 public class Field : CharField
 {
+    
+    
     public Field(char[][] fieldData) : base(fieldData)
     {
+        
     }
 
+    public Orientation Orientation { get; set; } = Orientation.NORTH;
+    
     public Field UpdateScore()
     {
         Score = 0;
@@ -28,15 +41,53 @@ public class Field : CharField
     {
         ApplyToAnyCharacter((c, _, _) => c == 'O', (c, x, y) =>
         {
-            var yy = y - 1;
-            while (yy >= 0 && FieldData[yy][x] == '.')
+            switch (Orientation)
             {
-                yy--;
+                case Orientation.NORTH:
+                    var yy = y - 1;
+                    while (yy >= 0 && FieldData[yy][x] == '.')
+                    {
+                        yy--;
+                    }
+
+                    yy++;
+
+                    Swap(x, y, x, yy);
+                    break;
+                case Orientation.WEST:
+                    var xx = x - 1;
+                    while (xx >= 0 && FieldData[y][xx] == '.')
+                    {
+                        xx--;
+                    }
+
+                    xx++;
+
+                    Swap(x, y, xx, y);
+                    break;
+                case Orientation.SOUTH:
+                    var yy2 = y + 1;
+                    while (yy2 < Height && FieldData[yy2][x] == '.')
+                    {
+                        yy2++;
+                    }
+
+                    yy2--;
+
+                    Swap(x, y, x, yy2);
+                    break;
+                case Orientation.EAST:
+                    var xx2 = x + 1;
+                    while (xx2 < Width && FieldData[y][xx2] == '.')
+                    {
+                        xx2++;
+                    }
+
+                    xx2--;
+
+                    Swap(x, y, xx2, y);
+                    break;
             }
-
-            yy++;
-
-            Swap(x, y, x, yy);
         });
        
         return this;
@@ -45,9 +96,13 @@ public class Field : CharField
     public Field Cycle()
     {
         for (var i = 0; i < 4; i++)
-        {
+        { 
             FallNorth();
-            RotateRight();
+           // RotateRight();
+           Orientation++;
+           if(Orientation > Orientation.WEST) {
+               Orientation = Orientation.NORTH;
+           }
         }
 
         UpdateHash();
@@ -78,27 +133,34 @@ class Solution : Solver
     {
         var field = ParseInput(input);
 
-        List<Field> history = [field];
+        //List<Field> history = [field];
+        Dictionary<ulong, int> scoreHistory = new();
+        List<ulong> history = new();
         
-        var cycleField = field;
+        history.Add(field.Hash);
+        scoreHistory[field.Hash] = field.Score;
+        
+        //var cycleField = field;
         const int maxIterations = 1_000_000_000;
         var index = -1;
 
         for (var i = 1; i < maxIterations; i++)
         {
-            cycleField = cycleField.Clone();
-            cycleField.Cycle();
+           // cycleField = cycleField.Clone();
+           // cycleField.Cycle();
+           field.Cycle();
 
-            var localCycleField = cycleField;
-            if (history.TryGetIndex(c => c.Hash == localCycleField.Hash, out index))
+            //var localCycleField = cycleField;
+            if (history.TryGetIndex(c => c == field.Hash, out index))
                 break;
 
-            history.Add(cycleField);
+            field.UpdateScore();
+            history.Add(field.Hash);
+            scoreHistory[field.Hash] = field.Score;
         }
 
         var resultIndex = (maxIterations - index) % (history.Count - index) + index;
-        return history[resultIndex].
-               UpdateScore().
-               Score;
+        var hash = history[resultIndex];
+        return scoreHistory[hash];
     }
 }
